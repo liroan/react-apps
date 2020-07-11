0daff87fcb00c4c5919f043d295ea4011afb1b95
liroan <serzhantov-02@mail.ru>
2020-07-11 23:30:15 +0300

    Updates

---
+++ precache-manifest.23d70aaea27eb10f758d580d767af69e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eabac308d69be9971a37b2eaca52b3",
+    "url": "/react-apps/index.html"
+  },
+  {
+    "revision": "94309f4c0eb8ce841fa8",
+    "url": "/react-apps/static/css/main.a9fb88c3.chunk.css"
+  },
+  {
+    "revision": "e11c8691b2e6f1720f4c",
+    "url": "/react-apps/static/js/2.6b98283e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-apps/static/js/2.6b98283e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94309f4c0eb8ce841fa8",
+    "url": "/react-apps/static/js/main.f8133849.chunk.js"
+  },
+  {
+    "revision": "edcbe99e77e3552cc7c2",
+    "url": "/react-apps/static/js/runtime-main.568588aa.js"
+  },
+  {
+    "revision": "4ac4c54a3934ea2287c0ba31a7cfb160",
+    "url": "/react-apps/static/media/cactus.4ac4c54a.png"
+  },
+  {
+    "revision": "2d4443d2351b93c3a734fcf0b06ed54f",
+    "url": "/react-apps/static/media/ch.2d4443d2.png"
+  },
+  {
+    "revision": "72e0b11b53bea0d2782d5991427aa213",
+    "url": "/react-apps/static/media/city.72e0b11b.png"
+  },
+  {
+    "revision": "0286b4e7c1c4c2ca21f3dd7689dcbe6c",
+    "url": "/react-apps/static/media/fr.0286b4e7.png"
+  },
+  {
+    "revision": "c6ed004cbadba2030c1a8970b78b8a82",
+    "url": "/react-apps/static/media/g4.c6ed004c.png"
+  },
+  {
+    "revision": "907d17f8080e94840f74c39ac73ebdef",
+    "url": "/react-apps/static/media/joystick.907d17f8.png"
+  },
+  {
+    "revision": "f3cd57a2a60cf458c6845febc9310bae",
+    "url": "/react-apps/static/media/mount8.f3cd57a2.png"
+  },
+  {
+    "revision": "6cf2048603eef043bb2ff6be46374241",
+    "url": "/react-apps/static/media/nebo.6cf20486.jpg"
+  },
+  {
+    "revision": "21e25971edfc5cdcadc5330b5bcb11aa",
+    "url": "/react-apps/static/media/pk.21e25971.png"
+  },
+  {
+    "revision": "d33d97b5216182155c396f9e2918afe5",
+    "url": "/react-apps/static/media/pngfuel.com.d33d97b5.png"
+  },
+  {
+    "revision": "6fec58874c892796452f785cda67b1a8",
+    "url": "/react-apps/static/media/smile.6fec5887.png"
+  },
+  {
+    "revision": "6901fe55ba7bd60821fe4eabab8cd64e",
+    "url": "/react-apps/static/media/website.6901fe55.png"
+  },
+  {
+    "revision": "31ccda779a5d53c29e7263cd8ca1cf19",
+    "url": "/react-apps/static/media/ррр3-removebg-preview.31ccda77.png"
+  },
+  {
+    "revision": "f37ce6f7a29df80b6b680b1c2ab9fbdd",
+    "url": "/react-apps/static/media/фффффффф-removebg-preview.f37ce6f7.png"
+  }
+]);
